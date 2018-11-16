68e2e0d52ec1f5a1fc750e8f2670374a082f78a9
Lewis Bails <lewis.bails@gmail.com>
2018-11-16 10:51:00 +0100

    Edits to FileReader

--- workshop_3/src/FileReader.java
+++ workshop_3/src/FileReader.java
@@ -12,27 +12,33 @@ public class FileReader
     *
     * @param filename Name of the text file (Ex. "prices.txt").
     */
-    public static void readInput(String filename) 
+    public static HashSet<Student> readInput(String filename)
     {
         //TODO: Create data structure of choice
         //Ex. List<Something> someList = new ArrayList<>();
+        List<Integer> grades = new ArrayList<>();
+        HashSet<Student> students = new HashSet<>();
 
         //Try opening the file and making a scanner that goes through the file
         try (Scanner scanner = new Scanner(new File(filename))) 
         {
             //Iterate through all the lines in the text file
-            while (scanner.hasNext()) 
+            while (scanner.hasNext())
             {
                 String line = scanner.nextLine();
                 System.out.println(line);
+                Student newStudent = null;
+                String newClass = "";
 
-                if (line.startsWith(">")) 
+                if (line.startsWith(">"))
                 {
                     //This line is the name of a student
                     String studentName = line.substring(1);
                     System.out.println("New student: " + studentName);
 
                     //TODO: Some code here
+                    newStudent = new Student(studentName);
+                    students.add(newStudent);
                 }
                 else if (!line.matches("\\d+"))
                 {
@@ -40,25 +46,30 @@ public class FileReader
                     System.out.println("Course: " + line);
 
                     //TODO: Some code here
+                    newClass = line;
+                    newStudent.addClass(newClass);
                 }
                 else
                 {
                     //This line is a grade
                     int grade = Integer.parseInt(line);
-                    
+
                     System.out.println("Grade: " + grade);
 
                     //TODO: Some code here
+                    newStudent.addGrade(newClass,grade);
                 }
             }
 
             //TODO: Return data structure of choice after while loop is done
+            return students;
             //Ex. return someList;
         } 
         catch (IOException ex) 
         {
             //File was probably not found :(
             System.out.println(ex.getMessage());
+            return null;
         }
     }
 
